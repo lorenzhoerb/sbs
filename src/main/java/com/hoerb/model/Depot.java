cef6ee5aa8ed3903f154de67e57c0c9bb23f173e
Lorenz Hörburger <lorenz.hoerburger@gmx.at>
2024-01-18 16:21:05 +0100

    add logging to Depot

--- src/main/java/com/hoerb/model/Depot.java
+++ src/main/java/com/hoerb/model/Depot.java
@@ -1,16 +1,20 @@
 package com.hoerb.model;
 
 import com.hoerb.exception.InsufficientFundsException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * The {@code Depot} class represents a financial depot associated with a user account.
  * It holds a balance and provides methods for withdrawing, depositing, and checking sufficient funds.
  */
 public class Depot {
+    private static final Logger logger = LogManager.getLogger();
     private double balance;
 
     public Depot(double balance) {
         this.balance = balance;
+        logger.info("Depot created with initial balance: €{}", balance);
     }
 
 
@@ -21,9 +25,14 @@ public class Depot {
      */
     public void withdraw(double amount) throws InsufficientFundsException {
         double newBalance = balance - amount;
-        if (newBalance < 0)
-            throw new InsufficientFundsException("Withdrawal failed: Insufficient funds. Your current balance is €" + balance);
+        if (newBalance < 0) {
+            String errorMessage = "Withdrawal failed: Insufficient funds. Your current balance is €" + balance;
+            logger.error(errorMessage);
+            throw new InsufficientFundsException(errorMessage);
+        }
         balance = newBalance;
+        logger.info("Withdrawal successful. Amount withdrawn: €{}, New balance: €{}", amount, balance);
+
     }
 
     /**
@@ -33,6 +42,7 @@ public class Depot {
      */
     public void deposit(double amount) {
         balance += amount;
+        logger.info("Deposit successful. Amount deposited: €{}, New balance: €{}", amount, balance);
     }
 
     /**
@@ -42,10 +52,13 @@ public class Depot {
      * @return {@code true} if the depot has sufficient funds, {@code false} otherwise.
      */
     public boolean hasSufficientFunds(double amount) {
-        return balance - amount >= 0;
+        boolean sufficientFunds = balance - amount >= 0;
+        logger.debug("Checking if depot has sufficient funds for €{}: {}", amount, sufficientFunds);
+        return sufficientFunds;
     }
 
     public double getBalance() {
+        logger.debug("Balance requested: €{}", balance);
         return balance;
     }
 }
